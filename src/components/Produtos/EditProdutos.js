800f5b4d86d9cc75c73fe72af05a6f8c9fa1383e
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2023-07-11 17:06:34 -0300

    Incrementando funções para o TableInfo
    Incrementando funções para o TableInfo + finalização da estilização do mesmo

--- src/components/Produtos/EditProdutos.js
+++ src/components/Produtos/EditProdutos.js
@@ -2,6 +2,10 @@ import InfoExtra from "./InfosExtra/InfoExtra";
 import TableInfo from "../TableInfo/TableInfo";
 
 export default function EditProdutos({styles, infoBoxExtra}) {
+    let dataAt = new Date();
+    dataAt = dataAt.getDate();
+    const infoTable = [{id: 1, nomeProd: "Prod1", qtdProd: 2, totValProd: 25, saldoAt: 2500, dataCompra: dataAt},
+                       {id: 2, nomeProd: "Prod2", qtdProd: 4, totValProd: 26, saldoAt: 2500, dataCompra: dataAt}];
     return (
         <>
             <h1>Edição de Produtos</h1>
@@ -13,7 +17,7 @@ export default function EditProdutos({styles, infoBoxExtra}) {
                 })}
 
             </div>
-            <TableInfo/>
+            <TableInfo title="Produtos" objectData={infoTable}/>
             
         </>
     )
--- src/components/TableInfo/TableInfo.js
+++ src/components/TableInfo/TableInfo.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import styles from '../../assets/css/TableInfo.module.css';
 import BtnListEdit from './BtnListEdit';
 
-export default function TableInfo({addBtn = null}) {
+export default function TableInfo(addBtn = true) {
+    const [isActiveAdd, setIsActiveAdd] = useState(0);
+
+    const activeAdd = () => {
+        setIsActiveAdd(!isActiveAdd);
+    }
 
     return (
         <div className={styles.tableContainer}>
@@ -28,25 +34,12 @@ export default function TableInfo({addBtn = null}) {
                             <BtnListEdit styles={styles}/>
                         </td>
                     </tr>
-                    <tr>
-                        <td>Info1</td>
-                        <td>Info2</td>
-                        <td>Info3</td>
-                        <td>Info4</td>
-                        <td className={styles.tableContent__btns}>
-                            {/* Adicionar uma verificação com o useState para saber quando deverá aparecer o botão de editar e quando deverá aparecer a coleção dos botões de confirmar e cancelar ação */}
-                            <BtnListEdit styles={styles}/> 
-                        </td>
-                    </tr>
-                </tbody>
-                {addBtn !== null ? (
-                    <tfoot>
-                        <button type='button' className={styles.addBtn}>Adicionar</button>
-                    </tfoot>
-                ) : null}
-                
-            
+                </tbody>            
             </table>
+            {addBtn !== null ? (
+                <button type='button' className={styles.addBtn} onClick={activeAdd}>{isActiveAdd ? "Cancelar" : "Adicionar novo Item à Tabela"}</button>        
+                ) : null
+            }
         </div>
     )
 }
