cc25f8cbaf05ed8a46644c5069f33060a6d9d805
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2023-07-11 17:07:45 -0300

    Tornando os dados da tabela dinâmicos
    Adição do map para percorrer os dados passados para o componente TableInfo e inserindo-os dentro da tabela

--- src/components/TableInfo/TableInfo.js
+++ src/components/TableInfo/TableInfo.js
@@ -2,38 +2,44 @@ import { useState } from 'react';
 import styles from '../../assets/css/TableInfo.module.css';
 import BtnListEdit from './BtnListEdit';
 
-export default function TableInfo(addBtn = true) {
+export default function TableInfo({addBtn = true, title, objectData}) {
     const [isActiveAdd, setIsActiveAdd] = useState(0);
 
     const activeAdd = () => {
         setIsActiveAdd(!isActiveAdd);
     }
 
+    const getKeysObject = (objValue) => {
+        const keyObject = objValue.map(value => Object.keys(value));
+        console.log(keyObject);
+        return keyObject[0];
+        
+    }
+
+    const keyObj = getKeysObject(objectData);
+
     return (
         <div className={styles.tableContainer}>
             <table>
                 <thead>
                     {/* Pegar tamanho do array/objetos de conteudo para centralizar */}
-                    <tr className={styles.tableTitle}><th colSpan={5}>NOME DA TABELA</th></tr>
+                    <tr className={styles.tableTitle}><th colSpan={keyObj.length + 1}>{title}</th></tr>
                     <tr>
-                        <th>Titulo de Conteudo1</th>
-                        <th>Titulo de Conteudo2</th>
-                        <th>Titulo de Conteudo3</th>
-                        <th>Titulo de Conteudo4</th>
+                        
+                        {keyObj.map((value, index) => <th key={index}>{value}</th>)}
                         <th>Ações</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Info1</td>
-                        <td>Info2</td>
-                        <td>Info3</td>
-                        <td>Info4</td>
-                        <td className={styles.tableContent__btns}>
-                            {/* Adicionar uma verificação com o useState para saber quando deverá aparecer o botão de editar e quando deverá aparecer a coleção dos botões de confirmar e cancelar ação */}
-                            <BtnListEdit styles={styles}/>
-                        </td>
-                    </tr>
+                    {objectData.map((objVal, index) => {
+                        const getValues = Object.values(objVal);
+                        return <tr key={index}>
+                                    {getValues.map((value, index) => <td key={index}>{value}</td>)}
+                                    <td className={styles.tableContent__btns}>
+                                        <BtnListEdit styles={styles}/>
+                                    </td>
+                               </tr>
+                    })}
                 </tbody>            
             </table>
             {addBtn !== null ? (
