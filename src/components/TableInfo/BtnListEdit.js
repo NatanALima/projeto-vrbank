1d833573ba12f2b2149a209fbd1823fe4ca02bad
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2023-10-08 16:28:19 -0300

    feat: adiciona animações
    - Adiciona arquivo de animações .css
    - Adiciona a classe das animações nos botões

--- src/components/TableInfo/BtnListEdit.js
+++ src/components/TableInfo/BtnListEdit.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {FaPencilAlt, FaCheck} from 'react-icons/fa';
-import {FaX} from 'react-icons/fa6'
+import {FaX} from 'react-icons/fa6';
+import '../../assets/css/animation.css';
 
 export default function BtnListEdit({styles}) {
     const [isEdit, setIsEdit] = useState(0);
@@ -13,12 +14,12 @@ export default function BtnListEdit({styles}) {
 
     return(
         <>
-            {!isEdit && <button type="button" className={styles.editBtn} onClick={handleEdit}><FaPencilAlt/></button>}
+            {!isEdit && <button type="button" className={`${styles.editBtn} fadeDownInBtn`} onClick={handleEdit}><FaPencilAlt/></button>}
             
             {isEdit ? (
                 <>
-                    <button type="button" className={styles.confirmBtn}><FaCheck/></button>
-                    <button type="button" className={styles.cancelBtn} onClick={handleEdit}><FaX/></button>
+                    <button type="button" className={`${styles.confirmBtn} fadeUpInBtn`}><FaCheck/></button>
+                    <button type="button" className={`${styles.cancelBtn} fadeUpInBtn`} onClick={handleEdit}><FaX/></button>
                 </>
             ) : null}
             
