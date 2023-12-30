ae1b8f01092d8af01008e90c575056b09d8c7510
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2023-12-30 01:34:50 -0300

    feat: Adiciona aba Banco

---
+++ src/components/Banco/Banco.js
@@ -0,0 +1,9 @@
+import styles from '../../assets/css/Banco.module.css';
+
+export default function Banco() {
+    return (
+        <section className={styles.banco}>
+            <h1>Banco</h1>
+        </section>
+    )
+}
