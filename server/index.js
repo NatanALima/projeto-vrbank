7b4d7e58198f7f79df59ce1c76094bdb2a0630c8
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-05-30 17:50:34 -0300

    fix(server): corrige erro de importação/exportação de módulos

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-import express, { json } from "express";
-
-import conn from './src/database/connect';
+import express from "express";
+import conn from "./src/database/connect.js";
+import Routes from "./src/routes/mainRouter.js";
 const app = express();
 const port = 8080;
 
 
 conn();
 
-app.use(json());
+app.use(express.json());
+app.use(Routes);
 app.listen(port, () => console.log("rodando"));
--- server/src/database/connect.js
+++ server/src/database/connect.js
@@ -1,15 +1,13 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 const urlDb = "mongodb+srv://root:D2Q4ci7tO1pN3p9t@projetovrbank.wxwvphb.mongodb.net/?retryWrites=true&w=majority&appName=projetoVrBank"
 
 
-const connectToDatabase = async () => {
+export default async function connectToDatabase() {
     try {
         await mongoose.connect(urlDb);
-        console.log('conectou Banco')
+        console.log('conectou Banco');
 
     } catch(err) {
         console.log('erro: ', err);
     }
 }
-
-module.exports = connectToDatabase;
