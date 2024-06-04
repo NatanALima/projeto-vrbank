c85aebc57bc4a85e8f4f873b4baad34fd1a5b9cb
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-06-04 20:45:40 -0300

    feat(server/auth): adiciona start de saldo ao efetuar o primeiro login

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
-import loginService from "../services/authService.js"
+import loginService from "../services/authService.js";
+import bancoService from "../services/bancoService.js";
 
 async function login(req, res) {
     const { userName, password } = req.body;
@@ -11,7 +12,16 @@ async function login(req, res) {
         
         const passIsValid = await bcrypt.compare(password, userInfo.password);
 
-        if(!passIsValid) return res.status(400).send({message: "Erro: Senha Incorreta!"})
+        if(!passIsValid) return res.status(400).send({message: "Erro: Senha Incorreta!"});
+
+
+        //inicialização do Banco (caso não tenha sido inicializada ainda)
+        const BancoInfo = await bancoService.getSaldoService(userInfo._id);
+
+        if(BancoInfo.length === 0) {
+            const startedBanco = await bancoService.bancoInit({user_ref: userInfo._id});
+            console.log(startedBanco);
+        }
 
         res.status(200).json(userInfo);
 
