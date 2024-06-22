38a693035cf11e49f1b74b93693f532ef7cb30e8
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-06-22 12:27:53 -0300

    feat(client/register): adiciona lógica de Cadastro de usuário

--- client/src/components/Autenticacao/RegisterForm.js
+++ client/src/components/Autenticacao/RegisterForm.js
@@ -1,25 +1,75 @@
+import { useState } from 'react';
 import '../../assets/css/animation.css';
 import InputAuth from '../layout/InputAuth';
-import { FaQuestion as IconQuestion} from 'react-icons/fa6';
+import { showSweetSuccessModal, showSweetErrorModal } from '../../models/SweetModal';
+import { registerService } from '../../services/user.service';
+import Cookies from 'js-cookie';
+import { useAuth } from '../../Context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function RegisterForm({styles}) {
+    const navigate = useNavigate();
+
+    const [userNameReg, setUserNameReg] = useState("");
+    const [passReg, setPassReg] = useState("");
+    const [repeatPassReg, setRepeatPassReg] = useState("");
+    const [invalidInfo, setInvalidInfo] = useState();
+
+    const { setUser } = useAuth();
+
+
+    const validatePass = (pass, repeatedPass) => {
+        return pass === repeatedPass
+    }
+
+    const handleRegister = async (e) => {
+        try {
+            e.preventDefault();
+
+            if (!validatePass(passReg, repeatPassReg)) {
+                setInvalidInfo({errorName: "passDifferent"});
+                return showSweetErrorModal("Erro de Cadastro!", "As senhas não coincidem!")
+            }
+            const res = await registerService({userName: userNameReg.toUpperCase(), password: passReg});
+            
+            showSweetSuccessModal("Cadastro Concluído", "Em segundos, você será redirecionado para a tela principal");
+            invalidInfo && setInvalidInfo(undefined);
+            
+            const info = JSON.stringify(res.data);
+            Cookies.set("userInfo", info, {expires: 1});
+            setUser(info);
+
+            navigate("/");
+            
+
+        } catch (err) {
+            const errInfo = err.response.data;
+            if(errInfo?.isError) {
+                showSweetErrorModal("Erro de Cadastro!", errInfo.errorMsg);
+                setInvalidInfo({errorName: errInfo.errorName});
     
-    const handleSubmit = (e) => {
-        e.preventDefault();
+            } else {
+                showSweetErrorModal("OPS!", "Algo deu Errado!");
+
+            }
+
+        }
+
+
     }
 
     return(
-        <form action="" className={styles.fadeChangeRight} onSubmit={handleSubmit}>
+        <form action="" className={styles.fadeChangeRight} onSubmit={handleRegister}>
             <h1>Cadastro</h1>
             <div className={styles.formInfo__specialInput}>
-                <InputAuth type={"text"} name={"userNameReg"} id={"userNameReg"} placeholder={"ex: user@VR2022"} textView={"Usuário"}/>
+                <InputAuth type={"text"} name={"userNameReg"} id={"userNameReg"} placeholder={"ex: user@VR2022"} textView={"Usuário*"} setInfo={setUserNameReg} isInvalidInfo={invalidInfo?.errorName === "userExists"}/>
                 <p className={styles.specialInput__msg}>
                     Utilize 
                     <strong>{" user@<Sigla Sala><ano de ingresso da sala>"}</strong>
                 </p>
             </div>
-            <InputAuth type={"text"} name={"userPassReg"} id={"userPassReg"} placeholder={"Digite uma Senha"} textView={"Senha"}/>
-            <InputAuth type={"text"} name={"userPassConfirmReg"} id={"userPassConfirmReg"} placeholder={"Confirme a Senha"} textView={"Confirmar Senha"}/>
+            <InputAuth type={"text"} name={"userPassReg"} id={"userPassReg"} placeholder={"Digite uma Senha"} textView={"Senha*"} setInfo={setPassReg}/>
+            <InputAuth type={"text"} name={"userPassConfirmReg"} id={"userPassConfirmReg"} placeholder={"Confirme a Senha"} textView={"Confirmar Senha*"} setInfo={setRepeatPassReg} isInvalidInfo={invalidInfo?.errorName === "passDifferent"}/>
             <button>Cadastrar</button>
         </form>
     )
--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -5,18 +5,14 @@ const baseURL = "http://localhost:8080";
 
 
 export async function registerService(body) {
-    try {
-        const res = await axios.post(`${baseURL}/user/create`, body);
-        return res;
+    const res = await axios.post(`${baseURL}/user/create`, body);
+    return res;
 
-    } catch(err) {
-        return err.response.data;
-    }
 }
 
 export async function loginService(body) {
-            const res = await axios.post(`${baseURL}/auth`, body);
-            return res;
+    const res = await axios.post(`${baseURL}/auth`, body);
+    return res;
 
 }
 
