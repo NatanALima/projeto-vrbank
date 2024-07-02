ab0ee5e5abc92436f0870e43ba6006bc25419a06
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-07-02 15:05:01 -0300

    fix(client/banco): corrige erro de recebimento de id de usuário
    - Verifica se o usuário está logado (Cookie de usuário setado) antes da conversão das informações para JSON

--- client/src/services/banco.service.js
+++ client/src/services/banco.service.js
@@ -4,14 +4,30 @@ import Cookies from 'js-cookie';
 const baseURL = "http://localhost:8080";
 
 export async function getBancoInfoService() {
-    const {_id} = JSON.parse(Cookies.get('userInfo'));
-
+    const {_id} = Cookies.get('userInfo') ?  JSON.parse(Cookies.get('userInfo')) : "";
     const res = await axios.get(`${baseURL}/banco/find`, {params: {idUser: _id}});
     return res;
+
 }
 
 export async function addSaldoBancoService(body) {
-    const {_id} = JSON.parse(Cookies.get('userInfo'));
+    const {_id} = Cookies.get('userInfo') ?  JSON.parse(Cookies.get('userInfo')) : "";
     const res = await axios.patch(`${baseURL}/banco/saldo/add`, body, {params: {idUser: _id}});
+    return res;
+
+}
+
+export async function removeSaldoBancoService(body) {
+    const {_id} = Cookies.get('userInfo') ?  JSON.parse(Cookies.get('userInfo')) : "";
+    const res = await axios.patch(`${baseURL}/banco/saldo/remove`, body, {params: {idUser: _id}});
+    return res;
+    
+}
+
+
+export async function addDespesaBancoService(body) {
+    const {_id} = Cookies.get('userInfo') ?  JSON.parse(Cookies.get('userInfo')) : "";
+
+    const res = await axios.patch(`${baseURL}/banco/divida/add`, body, {params: {idUser: _id}});
     return res;
 }
