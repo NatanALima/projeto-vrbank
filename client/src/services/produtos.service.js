7ca1fb80c50ab8cc6caae98d5894d6d34de1e1a9
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-07-10 16:05:12 -0300

    feat(client/produtos): adiciona consulta de produtos por usuário

--- client/src/services/produtos.service.js
+++ client/src/services/produtos.service.js
@@ -4,6 +4,12 @@ import Cookies from "js-cookie";
 const baseUrl = "http://localhost:8080";
 
 
+export async function getAllProdsByUserService() {
+    const {_id} = Cookies.get('userInfo') ? JSON.parse(Cookies.get('userInfo')) : "";
+
+    const res = await axios.get(`${baseUrl}/produtos/find`, {params: {idUser: _id}});
+    return res;
+}
 
 export async function addProdutosService(body) {
     const {_id} = Cookies.get('userInfo') ?  JSON.parse(Cookies.get('userInfo')) : "";
