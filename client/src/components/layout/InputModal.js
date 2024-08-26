b621a1b97665cdcba7479439d66ee112c97a49b2
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-08-25 23:18:34 -0300

    feat(InputModal): adiciona tipagem aos props do componente

--- client/src/components/layout/InputModal.js
+++ client/src/components/layout/InputModal.js
@@ -1,10 +1,27 @@
+import PropTypes from 'prop-types';
 import styles from '../../assets/css/InputModal.module.css'
 
-export default function InputModal({type, name, id, placeholder, textView, handleAction = null}) {
+export default function InputModal({type, name, id, value, placeholder, textView, handleAction}) {
     return(
         <div className={styles.inputContainer}>
             <label htmlFor={id}>{textView}</label>
-            <input type={type} name={name} id={id} placeholder={placeholder} required={true} onChange={(e) => handleAction(e)}/>
+            <input type={type} name={name} id={id} value={value} placeholder={placeholder} required={true} onChange={(e) => handleAction(e)}/>
         </div>
     )
+}
+
+InputModal.propTypes = {
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    placeholder: PropTypes.string.isRequired,
+    textView: PropTypes.string.isRequired,
+    handleAction: PropTypes.func
+
+}
+
+InputModal.defaultProps = {
+    value: "",
+    handleAction: null
 }
